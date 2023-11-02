79112be57091f2a73b034995b4ba4fdb59e06190
jmkim463 <126214989+jmkim463@users.noreply.github.com>
2023-11-02 10:10:29 +0900

    init

---
+++ src/main/java/com/travel/baenang/BaenangApplication.java
@@ -0,0 +1,13 @@
+package com.travel.baenang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaenangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaenangApplication.class, args);
+    }
+
+}
